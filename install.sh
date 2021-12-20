9d57c9d6ff6825afb7b419c2de187b07fe8bbea2
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-12-20 00:56:12 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+mkdir output
+go get golang.org/x/net/html ;  go get github.com/steelx/extractlinks ; go get github.com/bndr/gotabulate ; go get github.com/atotto/clipboard ; go get github.com/PuerkitoBio/goquery ; go get github.com/ArkAngeL43/port-scanning/portatotto/clipboard ; go get github.com/PuerkitoBio/goquery
